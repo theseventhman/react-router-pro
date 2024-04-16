240ac4d545205bf7bfe0f5c4a63b1b0323c7e9ba
tcn741 <tianchenlko@126.com>
2024-04-16 15:27:58 +0800

    初始化

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+
+//1.创建router实例对象并且配置路由对应关系
+
+const router = createBrowserRouter([
+ { path:'/login',
+  element:<div>我是登录页</div>
+  }
+,
+{
+  path:'/article',
+  element:<div>我是文章页</div>
+}])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
 
