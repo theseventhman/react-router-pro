99587a41675e03709950986791bc2c08bf609bd7
tcn741 <tianchenlko@126.com>
2024-04-20 09:05:31 +0800

    添加路由功能

--- src/index.js
+++ src/index.js
@@ -3,20 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import {RouterProvider } from 'react-router-dom';
 
-//1.创建router实例对象并且配置路由对应关系
+//1.导入路由router
 
-const router = createBrowserRouter([
- { path:'/login',
-  element:<div>我是登录页</div>
-  }
-,
-{
-  path:'/article',
-  element:<div>我是文章页</div>
-}])
-
+import router from './router';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
